c2caf33714853540dca43fdb01a1a266439e4832
GonzaJM <gonzalojamardotuc@gmail.com>
2025-05-27 12:39:54 -0300

    primer punto hecho

---
+++ ej2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class EntradaNumerica
+{
+    static void Main()
+    {
+        int num = 0;
+
+        do
+        {
+            Console.WriteLine("Escriba un número mayor que 0:");
+        } while (!int.TryParse(Console.ReadLine(), out num) || num < 1);
+
+        Console.WriteLine($"Ingresaste el número: {num}");
+    }
+}
