ba0c6062aba5677b6691f8e16a5d3caa132b74de
GonzaJM <gonzalojamardotuc@gmail.com>
2025-06-04 21:26:36 -0300

    invierto numeros

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class EntradaNumerica
+{
+    static void Main()
+    {
+        int num = 0, ultimo;
+
+        do
+        {
+            Console.WriteLine("Escriba un número mayor que 0:");
+        } while (!int.TryParse(Console.ReadLine(), out num) || num < 1);
+
+        Console.WriteLine($"Ingresaste el número: {num}");
+
+        if (num < 10) {
+            Console.Write(num);
+        } else {
+            while (num != 0)
+            {
+                ultimo = num % 10;
+                num = num / 10;
+                Console.Write(ultimo);
+            }
+        }
+    }
+}
